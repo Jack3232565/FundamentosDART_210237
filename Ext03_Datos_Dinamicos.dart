05b2815c3aba29fdfc6b742d2d83fb6434d0a7ab
Carlos.Crespo <210237@utxicotepec.edu.com>
2024-09-18 21:37:05 -0600

    Se resolvieron los errores de sintaxis para poder ejecutar los errorMessage

--- Ext03_Datos_Dinamicos.dart
+++ Ext03_Datos_Dinamicos.dart
@@ -1,33 +1,45 @@
-void main(){
+void main() {
   //Practica 05: Tipos de Datos Dinamicos (Dynamic - Comodín)
 
-  //Se puede declarar sin inicializard
+  //Se puede declarar sin inicializar
   dynamic errorMessage;
 
-  print(''' El error inicial de errorMessage es : $errorMessage
+  print('''El error inicial de errorMessage es: $errorMessage
   que por defecto permite establecer valores nulos''');
 
   //Ahora le asignamos un valor String
   errorMessage = "El usuario y contraseña son incorrectos";
-
-  errorMessage = 404;(''' ----------------------------------------------
+  print('''----------------------------------------------
   El nuevo valor de errorMessage es: $errorMessage
-  En algunos sistemas los errores son controlados por un codigo numerico.''');
+  En algunos sistemas los errores son controlados por un código numérico.''');
 
-  errorMessage = false;(''' ----------------------------------------------
+  //Ahora le asignamos un valor numérico
+  errorMessage = 404;
+  print('''----------------------------------------------
   El nuevo valor de errorMessage es: $errorMessage
-  Cuando el sistema fuciona correctamente el mensahe de error puede sr FALSO.''');
+  Cuando el sistema funciona correctamente, el mensaje de error puede ser FALSO.''');
 
-  errorMessage = [400,404,500,502,505];(''' ----------------------------------------------
+  //Ahora le asignamos un valor booleano
+  errorMessage = false;
+  print('''----------------------------------------------
   El nuevo valor de errorMessage es: $errorMessage
-  En caso de que el sistema detecte varios errores puedes agregar alguna lista''');
+  Cuando el sistema funciona correctamente, el mensaje de error puede ser FALSO.''');
 
-  errorMessage = {125, "Usuario no Encontrado", true, -1025.16 ,'A'};(''' ----------------------------------------------
+  //Ahora le asignamos una lista
+  errorMessage = [400, 404, 500, 502, 505];
+  print('''----------------------------------------------
   El nuevo valor de errorMessage es: $errorMessage
-  O inicluso como un conjunto  (SET) de datos''');
+  En caso de que el sistema detecte varios errores, puedes agregar una lista.''');
 
-  errorMessage = ()=>true;(''' ----------------------------------------------
-  El ultimo cambio de errorMensage es: $errorMessage
-  Cuando el sistema fuciona correctamente el mensahe de error puede sr FALSO.''');
+  //Ahora le asignamos un conjunto (Set)
+  errorMessage = {125, "Usuario no Encontrado", true, -1025.16, 'A'};
+  print('''----------------------------------------------
+  El nuevo valor de errorMessage es: $errorMessage
+  O incluso como un conjunto (SET) de datos.''');
 
+  //Finalmente le asignamos una función
+  errorMessage = () => true;
+  print('''----------------------------------------------
+  El último cambio de errorMessage es: $errorMessage
+  Cuando el sistema funciona correctamente, el mensaje de error puede ser FALSO.''');
 }
