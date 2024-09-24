120262060b713a3a633a8f0f3124018192aaacf2
Carlos.Crespo <210237@utxicotepec.edu.com>
2024-09-24 08:18:45 -0600

    Se ha creado el ejercicio 06

---
+++ Ext06_FuncionesyParametros.dart
@@ -0,0 +1,16 @@
+void main () {
+
+// 1. Llamado de un funcion sin prarámetros
+greetEveryone();
+
+}
+
+//Funciones y Parametros
+
+// Declaracion
+//1. Funcion sin Parametros
+
+void greetEveryone()
+{
+  print("Hola a Todas y Todos :D");
+}
